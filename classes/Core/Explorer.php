ab8661b8fcf3726a11daf74532700774461b1f69
cyrrill <cyrrill@users.noreply.github.com>
2014-11-22 08:52:34 +0100

    Updates filesystem helpers

--- classes/Core/Explorer.php
+++ classes/Core/Explorer.php
@@ -1,4 +1,5 @@
 <?php namespace Scale\Kernel\Core;
+
 /**
  * Explorer
  *
@@ -7,74 +8,74 @@
  * @author     Scale Team
  */
 
-trait Explorer {
+trait Explorer
+{
 
-	/**
-	 * 
-	 * @param type $pattern
-	 * @param type $flags
-	 * @return type
-	 */
-	public function rGlob($pattern, $flags = 0)
-	{
-		$files = glob($pattern, $flags);
+    /**
+     *
+     * @param type $pattern
+     * @param type $flags
+     * @return type
+     */
+    public function rGlob($pattern, $flags = 0)
+    {
+        $files = glob($pattern, $flags);
 
-		foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
+        foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
 
-		    $files = array_merge($files, $this->rGlob($dir.'/'.basename($pattern), $flags));
-		}
-		
-		return $files;
-	}
-	
-	/**
-	 * 
-	 * @param type $path
-	 * @return type
-	 */
-	public function recurse($path)
-	{
-		return $this->rGlob(App\PATH.$path);
-	}
+            $files = array_merge($files, $this->rGlob($dir.'/'.basename($pattern), $flags));
+        }
 
-	/**
-	 * 
-	 * @param type $path
-	 * @param type $file
-	 * @return type
-	 */
-	public function resolveFile($path, $file)
-	{
-		return str_replace(
-            '/', 
-            '\\', 
+        return $files;
+    }
+
+    /**
+     *
+     * @param type $path
+     * @return type
+     */
+    public function recurse($path)
+    {
+        return $this->rGlob(App\PATH.$path);
+    }
+
+    /**
+     *
+     * @param type $path
+     * @param type $file
+     * @return type
+     */
+    public function resolveFile($path, $file)
+    {
+        return str_replace(
+            '/',
+            '\\',
             str_replace(
-                '.php', 
-                '', 
+                '.php',
+                '',
                 str_replace(
-                    App\PATH.$path, 
-                    '', 
+                    App\PATH.$path,
+                    '',
                     $file
                 )
             )
         );
-	}
+    }
 
-	/**
-	 * Get application controller names
-	 * 
-	 * @return array
-	 */
-	public function getControllers($name='*')
-	{
-		$path = $this->recurse("/classes/Controller/$name.php");
+    /**
+     * Get application controller names
+     *
+     * @return array
+     */
+    public function getControllers($name = '*')
+    {
+        $path = $this->recurse("/classes/Controller/$name.php");
 
-		$names = array();
-		foreach( $path as $controller )
-		{
-			$names[] = $this->resolveFile('/classes/Controller/', $controller);
-		}
+        $names = array();
+        foreach ($path as $controller) {
+            $names[] = $this->resolveFile('/classes/Controller/', $controller);
+        }
 
-		return $names;
-	}
+        return $names;
+    }
 }
--- classes/Core/Finder.php
+++ classes/Core/Finder.php
@@ -1,4 +1,5 @@
 <?php namespace Scale\Kernel\Core;
+
 /**
  * File finder
  *
@@ -9,43 +10,27 @@
 
 trait Finder
 {
-
     /**
      * 
      * @param  path   $path  Relative locations to search for files
      * @param  string $scope 'application', 'kli', or none for full scope
      * @return array
      */
-    public function scan($path, $scope = null, $lowercase = false)
+    public function scan($path, $scope = null, $lowercase = false, $recursive = true)
     {
         $found = [];
 
-        $scopes_config = require \App\PATH.'/config/scopes.php';
+        foreach ($this->getDirs($scope) as $dir) {
 
-        if ($scope) {
-
-            $dirs = $scopes_config[$scope];
-
-        } else {
-
-            $dirs = [];
-
-            foreach ($scopes_config as $name => $paths) {
-
-                $dirs = array_merge($dirs, $paths);
+            $search_dir = \App\PATH.$dir.$path;
+            
+            if (!file_exists($search_dir)) {
+                continue;
             }
-        }
+            
+            $iterator = $this->getDirectoryIterator($search_dir, true);
 
-        foreach ($dirs as $dir) {
-
-            $search = $dir.'/'.$path;
-
-            $recursive = new \RecursiveIteratorIterator(
-                new \RecursiveDirectoryIterator($search),
-                \RecursiveIteratorIterator::SELF_FIRST
-            );
-
-            foreach ($recursive as $file) {
+            foreach ($iterator as $file) {
 
                 if ($file->isFile()) {
 
@@ -54,9 +39,50 @@ trait Finder
                     $found[] = ($lowercase) ? strtolower($name) : $name;
                 }
             }
-
         }
-
         return $found;
+    }
+
+    /**
+     * 
+     * @param  string $scope
+     * @return array
+     */
+    public function getDirs($scope)
+    {
+        $scopes_config = require \App\PATH.'/config/scopes.php';
+
+        if ($scope) {
+
+            $dirs = $scopes_config[$scope];
+        } else {
+
+            $dirs = [];
+            foreach ($scopes_config as $name => $paths) {
+                $dirs = array_merge($dirs, $paths);
+            }
+        }
+        return $dirs;
+    }
+    
+    /**
+     * 
+     * @param string $path
+     * @param bool   $recursive
+     * @return \IteratorIterator
+     */
+    public function getDirectoryIterator($path, $recursive = true)
+    {
+        if ($recursive) {
+            $iterator = new \RecursiveIteratorIterator(
+                new \RecursiveDirectoryIterator($path),
+                \RecursiveIteratorIterator::SELF_FIRST
+            );
+        } else {
+            $iterator = new \IteratorIterator(
+                new \DirectoryIterator($path)
+            );                
+        }   
+        return $iterator;
     }
 }
