318fa137426bc4055f0a8ecb4b5bfa107727d457
cyrrill <cyrrill@users.noreply.github.com>
2014-11-28 05:02:07 +0100

    Spaces in Factory

--- classes/Core/Factory.php
+++ classes/Core/Factory.php
@@ -5,14 +5,14 @@ use Scale\Kernel\Core\Container;
 class Factory extends Container
 {
     /**
-     * 
+     *
      * @param string $name
      * @return mixed
      */
     public function factory($name)
-    {    
+    {
         if (class_exists($name)) {
-            
+
             return $this->constructInject($name);
         }
     }
